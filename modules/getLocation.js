4b8976a120e6f13a073f1a2c2ce40e4722fb0bdc
dzhuleikhan <dzhuleikhan@gmail.com>
2024-10-15 22:27:43 +0700

    added CORS Proxy

--- modules/getLocation.js
+++ modules/getLocation.js
@@ -1,5 +1,5 @@
 export async function getLocation() {
-  let url = "https://ipinfo.io/json?token=fcd65e5fcfdda1";
+  let url = "https://corsproxy.io/?https://ipinfo.io/json?token=fcd65e5fcfdda1";
   let response = await fetch(url);
   let data = await response.json();
   return data;
