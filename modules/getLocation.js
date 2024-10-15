2a5b04b69dd8758beeb84117b454e920d3fa9b52
dzhuleikhan <dzhuleikhan@gmail.com>
2024-10-15 22:44:01 +0700

    Change geolocation service

--- modules/getLocation.js
+++ modules/getLocation.js
@@ -1,5 +1,8 @@
+const API_KEY = "e36d20c4-8c27-4d14-a0de-28ad9ccda291";
+
 export async function getLocation() {
-  let url = "https://ipinfo.io/json?token=fcd65e5fcfdda1";
+  // let url = "https://ipinfo.io/json?token=fcd65e5fcfdda1";
+  let url = "https://apiip.net/api/check?accessKey=" + API_KEY;
   let response = await fetch(url);
   let data = await response.json();
   return data;
