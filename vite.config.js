4aa49c25f529d0306b9a63248ac156768bb1eac4
dzhuleikhan <dzhuleikhan@gmail.com>
2024-12-06 15:11:19 +0300

    Second attempt

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,26 @@
 import { defineConfig, splitVendorChunkPlugin } from "vite";
+import { loadEnv } from "vite";
 
-export default defineConfig({
-  base: "",
-  esbuild: {
-    supported: {
-      "top-level-await": true,
-    },
-  },
-  server: {
-    proxy: {
-      "/api": {
-        target: "https://cdndigitaloceanspaces.cloud/",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
+export default defineConfig(({ mode }) => {
+  // Load environment variables based on the current mode
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    base: "",
+    esbuild: {
+      supported: {
+        "top-level-await": true,
       },
     },
-  },
-  plugins: [splitVendorChunkPlugin()],
+    server: {
+      proxy: {
+        "/api": {
+          target: env.VITE_API_URL, // Use the URL from the .env file
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ""),
+        },
+      },
+    },
+    plugins: [splitVendorChunkPlugin()],
+  };
 });
