a9c782e7ee7b28b297b57f6d40241a2d255d03bb
dzhuleikhan <dzhuleikhan@gmail.com>
2025-04-08 00:54:18 +0700

    Changed $ to ARS and removed Mexican Peso

--- modules/modalCurrency.js
+++ modules/modalCurrency.js
@@ -61,7 +61,7 @@ async function settingModalCurrency() {
     let locationData = await getLocation();
     let countryInput = locationData.countryCode;
 
-    if (countryInput === "RU") {
+    if (countryInput === "RU" || countryInput === "MX") {
       countryInput = "US";
     }
 
